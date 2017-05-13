830ce1c4e40a6158e256c99355c8840f373d6e9c
Woojgh <jamessalamonsen@gmail.com>
2017-05-12 18:56:40 -0700

    recording button

--- in_session.py
+++ in_session.py
@@ -14,21 +14,13 @@ class App:
         frame = Frame(master)
         frame.pack()
 
-        self.a = Button(frame, text="Record", command=self.Recording)
-        self.a.pack(side=RIGHT)
+        self.record_button = Button(frame, text="Record", command=self.Recording)
+        self.record_button.pack(side=RIGHT)
 
         self.button = Button(
             frame, text="QUIT", fg="red", command=frame.quit
             )
         self.button.pack(side=LEFT)
-
-        self.hi_there = Button(frame, text="Hello", command=self.say_hi)
-        self.hi_there.pack(side=LEFT)
-
-
-    def say_hi(self):
-        print ("hi there, everyone!")
-
 
     def Recording(self):
         messagebox.showinfo("Recording in Session","Recording is in session")
