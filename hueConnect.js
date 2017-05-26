adfb5bac23d78804ba4fa233b513a33e2904a9af
zdiehlio <zachary.diehl@gmail.com>
2017-05-26 10:08:41 -0700

    isntalled jshue library and re-working the functions

--- hueConnect.js
+++ hueConnect.js
@@ -1,21 +1,12 @@
 'use strict';
 
-var request = new XMLHttpRequest();
-var bridge = '#';
-var auth = '#';
+var hue = jsHue()
 
-function onState(light, state) {
-  request.open('PUT', "http://" + bridge + /api/ + auth + "/lights/" + light + "/state", true);
-  request.send('{"on":' + state + '}');
-}
-
-function offState(light, state) {
-  request.open('PUT', "http://" + bridge + /api/ + auth + "/lights/" + light + "/state", true);
-  request.send('{"off":' + state + '}');
-}
-
-function getBridges() {
-	request.open('GET',"https://www.meethue.com/api/nupnp", true);
-  bridge = request.responseText;
-	console.log(request.responseText);
-}
+hue.discover().then(bridges=> {
+  if(bridges.length === 0) {
+    console.log("Bridges not found");
+  }
+  else {
+    bridges.forEach(b => console.log('Bridge IP at %s', b.internalipaddress));
+  }
+}).catch(e => console.log('Error finding bridges', e));
